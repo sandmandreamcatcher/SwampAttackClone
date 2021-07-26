5c199bc68906b78eafaccea08a1c6cfb67a09bfa
sandmandreamcatcher <dr.catcher@ukr.net>
2021-07-26 20:12:24 +0300

    [add adreward]

---
+++ Assets/Scripts/AdsForWeapon.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+public class AdsForWeapon : MonoBehaviour, IUnityAdsListener
+{
+    [SerializeField] private GameObject watchAd;
+    [SerializeField] private Player _player;
+
+    private string gameId = "4233387";
+    private string myPlacementId = "Rewarded_Android";
+    private bool testMode = true;
+    private IUnityAdsListener _unityAdsListenerImplementation;
+
+
+    private void Start()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, testMode);
+
+        watchAd.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Advertisement.Show(myPlacementId);
+        //   watchAd.SetActive(false);
+        });
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        if (placementId == myPlacementId)
+        {
+            watchAd.SetActive(true);
+        }
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (showResult == ShowResult.Finished)
+        {
+            _player.AddMoney(5);
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+        }
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+    }
+}
---
+++ Assets/Scripts/AdsInit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsInit : MonoBehaviour
+{
+    private string gameId = "4233387";
+    private bool testMode = true;
+    
+    void Start()
+    {
+        Advertisement.Initialize(gameId,testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+    
+ IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady("Banner_Android"))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+    //    Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_RIGHT);
+        Advertisement.Banner.Show("Banner_Android");
+        
+    }
+}
